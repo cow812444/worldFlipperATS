9cf38566ef67449f2da4b6a70c6b89fbff52f9cb
cow812444 <cow812444@gmail.com>
2019-12-06 22:18:16 +0800

    initial commit

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from GitHub.worldFlipper_ATS.Controller import controller
+from GitHub.worldFlipperATS.Controller import controller
 import time
 
 #package:/data/app/air.jp.co.cygames.worldflipper-1/base.apk=air.jp.co.cygames.worldflipper
@@ -8,11 +8,11 @@ class Main:
         self.Controller = controller()
 
     def Test(self):
-        #self.Controller.Game_Start()
+        self.Controller.Game_Start()
         #time.sleep(7)
         #self.Controller.ADB.Touch(662,72)
         self.Controller.ADB.Game_ScreenHot_By_Adb(save_path='test.png')
-        self.Controller.ADB.Shut_Down_Game(Game_Activity_Name='air.jp.co.cygames.worldflipper')
+        #self.Controller.ADB.Shut_Down_Game(Game_Activity_Name='air.jp.co.cygames.worldflipper')
 
         #self.Controller.ADB.Game_ScreenHot_By_Adb()
 
